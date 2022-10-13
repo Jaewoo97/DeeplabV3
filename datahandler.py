c924858a3f0c2402d6f45e6fda67ea765e9012b2
jeong207@kaist.ac.kr <SKview108kaist!>
2022-10-13 17:01:13 +0900

    Inference for data mining

--- datahandler.py
+++ datahandler.py
@@ -1,10 +1,34 @@
 from pathlib import Path
-
+import numpy as np
 from torch.utils.data import DataLoader
 from torchvision import transforms
+import torch
 
 from segdataset import SegmentationDataset
 
+
+class normalize():
+    def __init__(self, mean = (0.485, 0.456, 0.406), std = (0.229, 0.224, 0.225)):
+
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, img):
+        imgarr = np.asarray(img)
+        proc_img = np.empty_like(imgarr, np.float32)
+
+        proc_img[..., 0] = (imgarr[..., 0] / 255. - self.mean[0]) / self.std[0]
+        proc_img[..., 1] = (imgarr[..., 1] / 255. - self.mean[1]) / self.std[1]
+        proc_img[..., 2] = (imgarr[..., 2] / 255. - self.mean[2]) / self.std[2]
+
+        return proc_img
+
+def HWC_to_CHW(tensor, sal=False):
+    if sal:
+        tensor = np.expand_dims(tensor, axis=0)
+    else:
+        tensor = np.transpose(tensor, (2, 0, 1))
+    return tensor
 
 def get_dataloader_sep_folder(data_dir: str,
                               image_folder: str = 'Image',
@@ -39,7 +63,11 @@ def get_dataloader_sep_folder(data_dir: str,
         dataloaders: Returns dataloaders dictionary containing the
         Train and Test dataloaders.
     """
-    data_transforms = transforms.Compose([transforms.ToTensor()])
+    tf_list = []
+    tf_list.append(normalize())
+    tf_list.append(HWC_to_CHW)
+    tf_list.append(torch.from_numpy)
+    data_transforms = transforms.Compose(tf_list)
 
     image_datasets = {
         x: SegmentationDataset(root=Path(data_dir) / x,
